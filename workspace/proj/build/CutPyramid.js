e751f223d33f703476528251258f692bb7113f60
Brunovsky <elbrunovsky@gmail.com>
2018-05-11 16:01:09 +0100

    Nothing

--- workspace/proj/build/CutPyramid.js
+++ workspace/proj/build/CutPyramid.js
@@ -81,7 +81,7 @@ class CutPyramid extends CGFobject
                 X = baseRadius * xUnit * (1 - s / stacks)
                     + topRadius * xUnit * (s / stacks);
                 Y = baseRadius * yUnit * (1 - s / stacks)
-                    + topRadius * yUnit * (s / stacks);
+                    + topRadius * yUnit * (s / stack s);
                 Z = s * stackHeight;
                 this.vertices.push(X, Y, Z); // v2U
                 this.vertices.push(X, Y, Z); // v2D
